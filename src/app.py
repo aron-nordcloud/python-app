08dd7e021e73926ee1b346357c3b47843a4fc2dc
Aron Jose Francis Amaldoss <aron.amaldoss@nordcloud.com>
2026-01-28 11:15:15 +0100

    more errors

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, big human! check cd runners after argocd',
+        'message': 'You are doing great, big human! check cd runners after argocd errors',
         'deployed_on': 'kubernetes'
     })
 
