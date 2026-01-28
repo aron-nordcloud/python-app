b9923b8bbbb2be1a0a54ec657e3fc6692d8cfc32
Aron Jose Francis Amaldoss <aron.amaldoss@nordcloud.com>
2026-01-28 11:35:21 +0100

    retry3

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, big human! check cd runners after argocd retry2',
+        'message': 'You are doing great, big human! check cd runners after argocd retry3',
         'deployed_on': 'kubernetes'
     })
 
